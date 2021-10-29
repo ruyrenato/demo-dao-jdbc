d695bff76077df178583da606967de1040d78183
renato <ruy.renato@gmail.com>
2021-10-28 21:55:57 -0300

    Reusando a instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,11 +7,10 @@ import model.entities.Seller;
 public class Program {
 	
 	public static void main(String[] args) {
-
+		
+		System.out.println("=== TEST 1: seller findById==");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 	}
 }
